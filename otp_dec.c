808617c0b73366499d141037a4cad4f1b1ded803
Jeff <muelljef@onid.oregonstate.edu>
2015-08-11 01:45:35 -0700

    decode server up and running, need to fix decode functionjj:w

---
+++ otp_dec.c
@@ -0,0 +1,223 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+#define BSIZE 256
+
+int checkFile(char *filename);
+void error(const char *msg);
+void removeNewline(char *buffer, int *n);
+
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n, cipherfd, keyfd;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    int numKey, numCipher;
+    FILE *fpKey;
+    char buffer[BSIZE];
+    char *rmLf;
+
+    //Checking that all parameters have been passed in
+    if (argc < 4) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+
+    //Checking that number of characters in Key file is 
+    //greater than ciphertext file
+    numCipher = checkFile(argv[1]);
+    if(numCipher < 0)
+    {
+        fprintf(stderr, "Error, invalid characters\n");
+        exit(1);
+    }
+    numKey = checkFile(argv[2]);
+    if (numKey < numCipher)
+    {
+        fprintf(stderr, "Key is not large enough\n");
+        exit(1);
+    }
+	
+    //convert the port number from string to socket
+    portno = atoi(argv[3]);
+    //Create the socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    
+    //Get server details for s_addr
+    server = gethostbyname("localhost");
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+
+    //Setting server details in struct serv_addr
+    memset((char *) &serv_addr, '\0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    memcpy((char *)&serv_addr.sin_addr.s_addr,
+            (char *)server->h_addr,
+            server->h_length);
+    serv_addr.sin_port = htons(portno);
+
+    //Connect to the socket
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+
+    //Initialize the buffer to nulls
+    memset(buffer, '\0', BSIZE);
+
+    //Send message indicated encode function calling
+    write(sockfd, "!!", 2);
+    read(sockfd, buffer, 2);
+    //If the incorrect string is returned, wrong server, exit
+    if (strncmp(buffer, "!!", 2) != 0)
+    {
+        fprintf(stderr, "Could not locate otp_dec_d\n");
+        close(sockfd);
+        exit(1);
+    }
+
+    //Open the ciphertext and key files for writing to socket
+    cipherfd = open(argv[1], O_RDONLY);
+    if (cipherfd < 0)
+    {
+        fprintf(stderr, "error opening file");
+        exit(1);
+    }
+    keyfd = open(argv[2], O_RDONLY);
+    if (fpKey < 0)
+    {
+        fprintf(stderr, "error opening key");
+        exit(1);
+    }
+
+    memset(buffer, '\0', BSIZE);
+    //write the ciphertext file to the socket
+    while((n = read(cipherfd, buffer, BSIZE - 1)) > 0)
+    {
+        //search for newline (from just before EOF) and remove it
+        rmLf = strchr(buffer, '\n');
+        if(rmLf != NULL)
+        {
+            *rmLf = '\0';
+            n--;
+        }
+        write(sockfd, buffer, n);
+        memset(buffer, '\0', BSIZE);
+    }
+    //write the delimiting character to the socket
+    write(sockfd, "&", 1);
+    //write the key file to the socket 
+    while((n = read(keyfd, buffer, BSIZE - 1)) > 0)
+    {
+        //search for newline (from just before EOF) and remove it
+        rmLf = strchr(buffer, '\n');
+        if(rmLf != NULL)
+        {
+            *rmLf = '\0';
+            n--;
+        }
+        write(sockfd, buffer, n);
+        memset(buffer, '\0', BSIZE);
+    }
+
+    //Shutdown the client write operation to send EOF to server
+    //to break read loop
+    if (shutdown(sockfd, SHUT_WR) < 0)
+        error("shutdown write client failed");
+
+    //PROCESS THE SERVER'S RESPONSE
+    memset(buffer, '\0', BSIZE);
+    //read the servers response and output the text
+    while((n = read(sockfd,buffer,BSIZE - 1)) > 0)
+    {
+        printf("%s",buffer);
+        memset(buffer, '\0', BSIZE);
+    }
+    close(sockfd);
+    printf("\n");
+    return 0;
+}
+
+//entry: error string such as calling function error
+//exit: will report msg with errno and exit with status 1
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+//entry: string and string length of n
+//exit: first newline will be replace by null terminating ch
+//      and n will be decremented
+void removeNewline(char *buffer, int *n)
+{
+    int i;
+    for(i = 0; i < *n ; i++)
+    {
+        if(buffer[i] == '\n')
+        {
+            buffer[i] = '\0';
+            *n--;
+        } 
+    }
+}
+
+//Entry: filename as string
+//Exit:  If all characters in file are valid the function will return
+//          the number of characters, if an invalid character is found
+//          the function will return -1
+int checkFile(char *filename)
+{
+    FILE *fp;
+    int c, count;
+
+    //open the file
+    fp = fopen(filename, "r");
+    if(fp == NULL) 
+    {
+        fprintf(stderr, "file %s did not open\n", filename);
+        return -1;
+    }
+
+    count = 0;
+    //loop through all the characters
+    while((c = fgetc(fp)) != EOF)
+    {
+        //If c is not a space or an alphabetic character
+        if(c != 32 && !isalpha(c))
+        {
+            //if c is a lf character
+            if(c == 10)
+            {
+                //get the next character
+                c = fgetc(fp);
+                //if newline is followed by EOF ok, return count
+                if (c == EOF)
+                {
+                    fclose(fp);
+                    return count;
+                }
+            }
+            //all other non space or alpha characters
+            //or newline not followed by EOF
+            //is invalid input
+            fclose(fp);
+            return -1;
+        }
+        count++;
+    }
+    //in case the file happens to end in just EOF without newline prior
+    fclose(fp);
+    return count;
+}
---
+++ otp_dec_d.c
@@ -0,0 +1,298 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <syslog.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+#define BSIZE 256 
+
+void error(const char *msg);
+void removeNewline(char *buffer, int *n);
+int splitRead(char *buffer, int n, FILE *org, FILE *key);
+void decode(char *org, char *key, int n);
+void decipherResponse(int newsockfd, FILE *org, FILE *key);
+void handleResponse(int newsockfd);
+
+//From TLPI
+void clearChildren(int sig)
+{
+    //save the errno before calling waitpid
+    int savedErrno;
+    savedErrno = errno;
+    //clear the waiting children
+    while(waitpid(-1, NULL, WNOHANG) > 0)
+        continue;
+    //restore the errno
+    errno = savedErrno;
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, newsockfd, portno;
+    struct sockaddr_in serv_addr;
+    int n;
+    
+    //Checking enough arguments given
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+
+    //SIG_CHLD handler to wait on children
+    struct sigaction sa;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    sa.sa_handler = &clearChildren;
+    if(sigaction(SIGCHLD, &sa, NULL) == -1)
+    {
+        error("sigaction failure");
+    }
+
+    //Creating the socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+       error("ERROR opening socket");
+
+    //Setting the server address to accept any clients
+    memset((char *) &serv_addr, '\0', sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+
+    //Binding the socket
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+         error("ERROR on binding");
+
+    //Listening on the socket
+    listen(sockfd,5);
+ 
+    //SERVER LOOP
+    //infinite loop waiting for clients 
+    while(1)
+    {
+        //getting new clients
+        newsockfd = accept(sockfd, NULL, NULL);
+        if (newsockfd < 0) 
+        {
+            perror("accept");
+            continue;
+        }
+
+        //Fork the client their own process, and return to listen
+        pid_t spawnpid;
+        spawnpid = fork(); 
+        if(spawnpid == 0)
+        {
+            close(sockfd);         //not needed copy of listening socket
+            //handle the server response
+            handleResponse(newsockfd);
+            exit(0);
+        }
+        else if (spawnpid > 0)
+        {
+            //close the extra copy of data socket
+            close(newsockfd);
+        }
+        else
+        {
+            fprintf(stderr, "fork failed, closing client\n");
+            //close the extra copy of data socket
+            close(newsockfd);
+        }
+    }
+    return 0; 
+}
+
+//entry: message to display with perror
+//exit: will display perror message then exit with status 1
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+//entry: string and address of string length n
+//exit: It will remove the first newline found and decrement n
+void removeNewline(char *buffer, int *n)
+{
+    int i;
+    for(i = 0; i < *n ; i++)
+    {
+        if(buffer[i] == '\n')
+        {
+            buffer[i] = '\0';
+            *n--;
+        } 
+    }
+}
+
+//entry: buffer is split bye &, int is number of characters in the buffer
+//      org is the plaintext file, and key is the key file
+//exit: text before & will be written to plaintext, and text after will
+//      be written to key file
+int splitRead(char *buffer, int n, FILE *org, FILE *key)
+{
+    int i;
+    char *pch;
+    //iterate through buffer looking for &
+    for(i = 0; i < n; i++)
+    {
+        if(buffer[i] == '&')
+        {
+            //when fount take first token and write to org file
+            pch = strtok(buffer, "&");
+            fprintf(org, "%s", pch);
+            //take second token and write to key file
+            pch = strtok(NULL, "&");
+            fprintf(key, "%s", pch);
+            //clear the buffer and return 1 to indicate found
+            memset(buffer, '\0', BSIZE);
+            return 1;
+        }
+    }
+    //'&' not found, write buffer to org file
+    //return 0 to indicate & was not found
+    fprintf(org, "%s", buffer);
+    memset(buffer, '\0', BSIZE);
+    return 0;
+}
+
+// entry: org string to decode and key string to use for encoding,
+//      key must be as long as org. n is the length of org string
+// exit: org will be decoded text based on key string
+//         0 is space +32, 1-26 is [A-Z] +64
+void decode(char *org, char *key, int n)
+{
+    int i, tmpch;
+    //loop through all characters
+    for(i = 0; i < n; i++)
+    {
+        //convert characters to integer and add org to key at i
+        tmpch = (int)org[i] - (int)key[i];
+        //take the modulus of 27
+        tmpch = tmpch % 27;
+        //case 0 is the space, so if 0 set to space
+        //otherwise add 64 to the integer and cast to character
+        switch(tmpch)
+        {
+            case 0:
+                org[i] = ' ';
+                break;
+            default:
+                org[i] = (char)(tmpch + 64);
+        }
+    }
+}
+
+//entry: a valid socket stream  with the client to transfer data on, and open 
+//      ciphertext and key files
+//exit; ciphertext is encrypted based on key text and sent over
+//      the socket
+void decipherResponse(int newsockfd, FILE *org, FILE *key)
+{
+    int n;
+    char buffer[BSIZE], keyBuffer[BSIZE];
+    //clear the buffers
+    memset(buffer, '\0', BSIZE);
+    memset(keyBuffer, '\0', BSIZE);
+
+    //SERVER RESPONDING WITH CIPHER TEXT
+    //get BSIZE characters from plaintext
+    while((fgets(buffer, BSIZE, org)) != NULL)
+    {
+        //get BSIZE characters from keyBuffer
+        if((fgets(keyBuffer, BSIZE, key)) == NULL)
+            fprintf(stderr, "inside response: key not large enough\n");
+        //get the string size of the buffer
+        n = strnlen(buffer, BSIZE);
+        //remove the newline if it is there
+        //adjust the size n
+        removeNewline(buffer, &n);
+        //decode the buffer using the keyBuffer
+        decode(buffer, keyBuffer, n);
+        //write to the socket
+        write(newsockfd, buffer, n);
+        //clear the buffers
+        memset(buffer, '\0', BSIZE);
+        memset(keyBuffer, '\0', BSIZE);
+    }
+}
+
+//entry: a valid open socket with the client
+//exit: will verify connecting with correct client,
+//      then recevie message, save to temp file, use
+//      temp files to decode text and send it back to
+//      client
+//      
+void handleResponse(int newsockfd)
+{
+
+    FILE *org, *key;
+    char buffer[BSIZE];
+    int n, i, keyFlag;
+    char temp[BSIZE], temp2[BSIZE], pidStr[BSIZE];
+
+    //ensure clear buffer
+    memset(buffer, '\0', BSIZE);
+    memset(pidStr, '\0', BSIZE);
+    memset(temp, '\0', BSIZE);
+    memset(temp2, '\0', BSIZE);
+
+    //ensure contacting the decode function
+    n = read(newsockfd, buffer, 2);
+    write(newsockfd, "!!", 2);
+    if (strncmp(buffer, "!!", 2) != 0)
+        return;
+
+    //Set temp file names
+    sprintf(pidStr, "%d", (int)getpid());
+    strncat(temp, "org_", BSIZE);
+    strncat(temp, pidStr, BSIZE  - strnlen(temp, BSIZE));
+    strncat(temp2, "key_", BSIZE);
+    strncat(temp2, pidStr, BSIZE  - strnlen(temp, BSIZE));
+    
+    //open a file to write content from stream
+    org = fopen(temp, "w+");
+    key = fopen(temp2, "w+");
+
+    //initialize bool for key input being sent to false
+    //plaintext send first, then key input after flag
+    keyFlag = 0;
+    while((n = read(newsockfd,buffer,BSIZE - 1)) > 0)
+    {
+        if (keyFlag == 0)
+        {
+            //splitRead writes to files, to plaintext before '&'
+            //and key to after
+            keyFlag = splitRead(buffer, n, org, key);   
+        }
+        else
+        {
+            //all the rest is written to the key
+            fprintf(key, "%s", buffer);
+            memset(buffer, '\0', BSIZE);
+        }
+    }
+
+    //rewind the files to start from beginning
+    rewind(org);
+    rewind(key);
+    
+    //call the decipherResponse to decode and send the message
+    decipherResponse(newsockfd, org, key);
+
+    //close the files
+    fclose(org);
+    fclose(key);
+    //delete the temp files
+    remove(temp);
+    remove(temp2);
+}
