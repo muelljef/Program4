c112c6af37b74a155f49ebb70e568fbc73063bdc
Jeff <muelljef@onid.oregonstate.edu>
2015-08-10 23:48:17 -0700

    good temp files working

--- otp_enc.c
+++ otp_enc.c
@@ -9,11 +9,12 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
-#include "shared.h"
 
-#ifndef BSIZE
 #define BSIZE 256
-#endif
+
+int checkFile(char *filename);
+void error(const char *msg);
+void removeNewline(char *buffer, int *n);
 
 int main(int argc, char *argv[])
 {
@@ -134,4 +135,71 @@ int main(int argc, char *argv[])
     close(sockfd);
     printf("\n");
     return 0;
+}
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+//buffer and string length n
+void removeNewline(char *buffer, int *n)
+{
+    int i;
+    for(i = 0; i < *n ; i++)
+    {
+        if(buffer[i] == '\n')
+        {
+            buffer[i] = '\0';
+            *n--;
+        } 
+    }
+}
+
+//Entry: filename as string
+//Exit:  If all characters in file are valid the function will return
+//          the number of characters, if an invalid character is found
+//          the function will return -1
+int checkFile(char *filename)
+{
+    FILE *fp;
+    int c, count;
+
+    fp = fopen(filename, "r");
+    if(fp == NULL) 
+    {
+        fprintf(stderr, "file %s did not open\n", filename);
+        return -1;
+    }
+
+    count = 0;
+    while((c = fgetc(fp)) != EOF)
+    {
+        //If c is not a space or an alphabetic character
+        if(c != 32 && !isalpha(c))
+        {
+            //if c is a lf character
+            if(c == 10)
+            {
+                //get the next character
+                c = fgetc(fp);
+                //if newline is followed by EOF ok, return count
+                if (c == EOF)
+                {
+                    fclose(fp);
+                    return count;
+                }
+            }
+            //all other non space or alpha characters
+            //or newline not followed by EOF
+            //is invalid input
+            fclose(fp);
+            return -1;
+        }
+        count++;
+    }
+    //in case the file happens to end in just EOF without newline prior
+    fclose(fp);
+    return count;
 }
--- otp_enc_d.c
+++ otp_enc_d.c
@@ -1,5 +1,3 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,13 +11,14 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
-#include "shared.h"
-#include "serverFunc.h"
-
-#ifndef BSIZE
 #define BSIZE 256 
-#endif
 
+void error(const char *msg);
+void removeNewline(char *buffer, int *n);
+int splitRead(char *buffer, int n, FILE *org, FILE *key);
+void encode(char *org, char *key, int n);
+void cipherResponse(int newsockfd, FILE *org, FILE *key);
+void handleResponse(int newsockfd);
 
 //From TLPI
 void clearChildren(int sig)
@@ -87,8 +86,6 @@ int main(int argc, char *argv[])
         if(spawnpid == 0)
         {
             close(sockfd);         //not needed copy of listening socket
-            //check encode function is calling
-            
             //handle the server response
             handleResponse(newsockfd);
             exit(0);
@@ -103,4 +100,162 @@ int main(int argc, char *argv[])
         }
     }
     return 0; 
+}
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+//buffer and string length n
+void removeNewline(char *buffer, int *n)
+{
+    int i;
+    for(i = 0; i < *n ; i++)
+    {
+        if(buffer[i] == '\n')
+        {
+            buffer[i] = '\0';
+            *n--;
+        } 
+    }
+}
+
+//entry: buffer is split bye &, int is number of characters in the buffer
+//      org is the plaintext file, and key is the key file
+//exit: text before & will be written to plaintext, and text after will
+//      be written to key file
+int splitRead(char *buffer, int n, FILE *org, FILE *key)
+{
+    int i;
+    for(i = 0; i < n; i++)
+    {
+        if(buffer[i] == '&')
+        {
+            char *pch;
+            pch = strtok(buffer, "&");
+            fprintf(org, "%s", pch);
+            pch = strtok(NULL, "&");
+            fprintf(key, "%s", pch);
+            memset(buffer, '\0', BSIZE);
+            return 1;
+        }
+    }
+    //'&' not found
+    fprintf(org, "%s", buffer);
+    memset(buffer, '\0', BSIZE);
+    return 0;
+}
+
+// 0 is space +32, 1-26 is [A-Z] +64
+void encode(char *org, char *key, int n)
+{
+    int i, tmpch;
+    for(i = 0; i < n; i++)
+    {
+        tmpch = (int)org[i] + (int)key[i];
+        tmpch = tmpch % 27;
+        switch(tmpch)
+        {
+            case 0:
+                org[i] = ' ';
+                break;
+            default:
+                org[i] = (char)(tmpch + 64);
+        }
+    }
+}
+
+void cipherResponse(int newsockfd, FILE *org, FILE *key)
+{
+    int n;
+    char buffer[BSIZE], keyBuffer[BSIZE];
+    //clear the buffers
+    memset(buffer, '\0', BSIZE);
+    memset(keyBuffer, '\0', BSIZE);
+
+    //SERVER RESPONDING WITH CIPHER TEXT
+    //get BSIZE characters from plaintext
+    while((fgets(buffer, BSIZE, org)) != NULL)
+    {
+        //get BSIZE characters from keyBuffer
+        if((fgets(keyBuffer, BSIZE, key)) == NULL)
+            error("key not large enough");
+        //get the string size of the buffer
+        n = strnlen(buffer, BSIZE);
+        //remove the newline if it is there
+        //adjust the size n
+        removeNewline(buffer, &n);
+        //encode the buffer using the keyBuffer
+        encode(buffer, keyBuffer, n);
+        //write to the socket
+        write(newsockfd, buffer, n);
+        //clear the buffers
+        memset(buffer, '\0', BSIZE);
+        memset(keyBuffer, '\0', BSIZE);
+    }
+}
+
+void handleResponse(int newsockfd)
+{
+
+    FILE *org, *key;
+    char buffer[BSIZE];
+    int n, i, keyFlag;
+    char temp[BSIZE], temp2[BSIZE], pidStr[BSIZE];
+
+    //ensure clear buffer
+    memset(buffer, '\0', BSIZE);
+    memset(pidStr, '\0', BSIZE);
+    memset(temp, '\0', BSIZE);
+    memset(temp2, '\0', BSIZE);
+
+    //ensure contacting the encode function
+    n = read(newsockfd, buffer, 2);
+    write(newsockfd, "@@", 2);
+    if (strncmp(buffer, "@@", 2) != 0)
+        return;
+
+    sprintf(pidStr, "%d", (int)getpid());
+    strncat(temp, "org_", BSIZE);
+    strncat(temp, pidStr, BSIZE  - strnlen(temp, BSIZE));
+    strncat(temp2, "key_", BSIZE);
+    strncat(temp2, pidStr, BSIZE  - strnlen(temp, BSIZE));
+    
+    //open a file to write content from stream
+    org = fopen(temp, "w+");
+    key = fopen(temp2, "w+");
+
+    //initialize bool for key input being sent to false
+    //plaintext send first, then key input after flag
+    keyFlag = 0;
+    while((n = read(newsockfd,buffer,BSIZE - 1)) > 0)
+    {
+        if (keyFlag == 0)
+        {
+            //splitRead writes to files, to plaintext before '&'
+            //and key to after
+            keyFlag = splitRead(buffer, n, org, key);   
+        }
+        else
+        {
+            //all the rest is written to the key
+            fprintf(key, "%s", buffer);
+            memset(buffer, '\0', BSIZE);
+        }
+    }
+
+    //rewind the files to start from beginning
+    rewind(org);
+    rewind(key);
+    
+    //call the cipherResponse to encode and send the message
+    cipherResponse(newsockfd, org, key);
+
+    //close the files
+    fclose(org);
+    fclose(key);
+    remove(temp);
+    remove(temp2);
 }
